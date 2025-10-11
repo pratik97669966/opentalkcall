4770da2fdc01c7d058210be252b019d1915c79de
Boss <officialopentalk@gmail.com>
2025-10-12 00:12:46 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -26,7 +26,7 @@ const peer = new Peer(undefined, {
     iceServers: [
       { url: "stun:stun.l.google.com:19302" },
       {
-        url: "turn:relay1.expressturn.com:3480?transport=tcp",
+        url: "turn:relay1.expressturn.com:3480",
         username: "000000002075222842",
         credential: "giRm2TkcSMy0xCw97M7YdGpOD10=",
       },
