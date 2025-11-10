3161ce14f34da51c492104d8c9ec9822f36a3838
Boss <officialopentalk@gmail.com>
2025-11-10 15:39:17 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,8 +17,6 @@ document.querySelector(".main__right").style.display = "flex";
 document.querySelector(".main__right").style.flex = "1";
 document.querySelector(".main__left").style.display = "none";
 
-// PeerJS setup
-// PeerJS setup
 const peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
@@ -26,38 +24,29 @@ const peer = new Peer(undefined, {
   secure: true,
   config: {
     iceServers: [
-     { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun.l.google.com:19302" },
       { urls: "stun:stun1.l.google.com:19302" },
       { urls: "stun:stun2.l.google.com:19302" },
       { urls: "stun:stun3.l.google.com:19302" },
       { urls: "stun:stun4.l.google.com:19302" },
       { urls: "stun:stun.services.mozilla.com" },
-{
-        urls: "stun:stun.relay.metered.ca:80",
-      },
+      { urls: ["stun:bn-turn1.xirsys.com"] },
       {
-        urls: "turn:global.relay.metered.ca:80",
-        username: "bfe25cb478e9db8dca0f9936",
-        credential: "PQw/gBqH9vMUM4He",
-      },
-      {
-        urls: "turn:global.relay.metered.ca:80?transport=tcp",
-        username: "bfe25cb478e9db8dca0f9936",
-        credential: "PQw/gBqH9vMUM4He",
-      },
-      {
-        urls: "turn:global.relay.metered.ca:443",
-        username: "bfe25cb478e9db8dca0f9936",
-        credential: "PQw/gBqH9vMUM4He",
-      },
-      {
-        urls: "turns:global.relay.metered.ca:443?transport=tcp",
-        username: "bfe25cb478e9db8dca0f9936",
-        credential: "PQw/gBqH9vMUM4He",
-      },
+        username: "DbKPwfXrz4KtlM7FpK6d5QXPGyl_p9iMw33FYW6x7YENX93zZTbYvnX4LB7RGiGnAAAAAGkGBPNnb21lcGlmODk4",
+        credential: "0e384d54-b723-11f0-a364-0242ac140004",
+        urls: [
+          "turn:bn-turn1.xirsys.com:80?transport=udp",
+          "turn:bn-turn1.xirsys.com:3478?transport=udp",
+          "turn:bn-turn1.xirsys.com:80?transport=tcp",
+          "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+          "turns:bn-turn1.xirsys.com:443?transport=tcp",
+          "turns:bn-turn1.xirsys.com:5349?transport=tcp"
+        ]
+      }
     ]
   }
 });
+
 
 let myVideoStream;
 
