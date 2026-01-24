e6c4135ce10a298c88db8b725a2f0eda7c4c5e74
Boss <officialopentalk@gmail.com>
2026-01-25 01:21:25 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -43,23 +43,23 @@ const peer = new Peer(undefined, {
       // Metered TURN
 {
         urls: "turn:global.relay.metered.ca:80",
-        username: "58830d927b8c607836cbb097",
-        credential: "6cD/L6sm45+jiWaA",
+        username: "47c27e26f297ef10e0e14f8e",
+        credential: "Wgtw82jdFiVhl4Zn",
       },
       {
         urls: "turn:global.relay.metered.ca:80?transport=tcp",
-        username: "58830d927b8c607836cbb097",
-        credential: "6cD/L6sm45+jiWaA",
+        username: "47c27e26f297ef10e0e14f8e",
+        credential: "Wgtw82jdFiVhl4Zn",
       },
       {
         urls: "turn:global.relay.metered.ca:443",
-        username: "58830d927b8c607836cbb097",
-        credential: "6cD/L6sm45+jiWaA",
+        username: "47c27e26f297ef10e0e14f8e",
+        credential: "Wgtw82jdFiVhl4Zn",
       },
       {
         urls: "turns:global.relay.metered.ca:443?transport=tcp",
-        username: "58830d927b8c607836cbb097",
-        credential: "6cD/L6sm45+jiWaA",
+        username: "47c27e26f297ef10e0e14f8e",
+        credential: "Wgtw82jdFiVhl4Zn",
       },
     ],
   },
